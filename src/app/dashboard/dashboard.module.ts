0116b3202d1fd8c71566e39c7a55dc539ce47655
valenti-casas-ilerna <vcasas@ilerna.com>
2022-07-06 22:19:27 +0200

    Creado badge

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -8,12 +8,13 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule  } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { BadgeComponent } from './components/badge/badge.component';
 
 
 
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [DashboardComponent, BadgeComponent],
   imports: [
     CommonModule,
     DashboardRoutingModule,
