c8d4297f56c5a0298259f8a9306fac9557253ab8
valenti-casas-ilerna <vcasas@ilerna.com>
2022-07-06 23:27:47 +0200

    Added all badges to the tree

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -13,7 +13,12 @@ export class DashboardComponent implements OnInit, OnDestroy {
 
   mobileQuery: MediaQueryList;
 
-  dartBadges = Array.from({length: 5}, (_, i) => `assets/images/badges/Dart ${i + 1}.png`);
+  badgesUrl: string = 'assets/images/badges/';
+  dartBadges = Array.from({length: 5}, (_, i) => `${this.badgesUrl}Dart ${i + 1}.png`);
+  flutterBadges = Array.from({length: 5}, (_, i) => `${this.badgesUrl}Flutter ${i + 1}.png`);
+  cSharpBadges = Array.from({length: 5}, (_, i) => `${this.badgesUrl}CSharp ${i + 1}.png`);
+  unityBadges = Array.from({length: 5}, (_, i) => `${this.badgesUrl}Unity ${i + 1}.png`);
+  firebaseBadges = Array.from({length: 5}, (_, i) => `${this.badgesUrl}Firebase ${i + 1}.png`);
 
   fillerNav = Array.from({length: 2}, (_, i) => `Nav Item ${i + 1}`);
 
