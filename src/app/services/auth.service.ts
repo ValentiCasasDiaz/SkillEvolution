8d65333ed0bac707f917127b835a3070eb54cd06
valenti-casas-ilerna <vcasas@ilerna.com>
2022-07-06 22:11:14 +0200

    refactored

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,21 +1,14 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from "@angular/fire/auth";
-import { Observable } from 'rxjs';
 import { LoginData } from '../interfaces/login-data.interface';
 
 import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
-
 
 @Injectable({
   providedIn: 'root'
 })
 
-//https://www.positronx.io/firebase-authentication-in-angular-8-with-angularfire2/
-//https://betterprogramming.pub/angular-13-firebase-authentication-tutorial-with-angularfire-7-23dc8cee42c4
 export class AuthService {
-
 
   constructor(private auth: AngularFireAuth) {
   }
